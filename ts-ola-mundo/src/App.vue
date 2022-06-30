3e7e9d47262e999d276102a105e45f4351593c6b
Henrique-Terres-Dell <Henrique.Terres@Dellteam.com>
2022-06-30 13:32:58 -0300

    Ola mundo vue

---
+++ ts-ola-mundo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Comentarista nome="Henrique Terres"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Comentarista from './components/Comentarista.vue';
+
+@Component({
+  components: {
+    Comentarista,
+  },
+})
+export default class App extends Vue {}
+</script>
---
+++ ts-ola-mundo/src/components/Comentarista.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button @click="comentar">
+            Comentar
+        </button>
+        <p>
+            {{ comentario }}
+        </p>
+        <p>
+            {{ detalhes }}
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import { Prop } from 'vue-property-decorator'
+
+@Component
+export default class Comenatarista extends Vue {
+    private comentario = ''
+    @Prop() private nome: string
+    
+    private comentar () : void {
+        this.comentario = 'Ola Mundo'
+    }
+
+    get detalhes() : string {
+        return `${this.nome} comentou: ${this.comentario}`
+    }
+}
+
+</script>
